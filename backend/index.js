6e4a6182f5dfab01827d99fdc2503b93e97c727c
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-09-06 18:41:42 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { connectDB } = require("./config/db");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", );
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+connectDB();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
