cfce3438005a78052911f133e0f9c3f0279fb82f
Nikhilsethi7668 <nikhilsethin494@gmail.com>
2025-09-08 04:12:50 +0530

    updated relation

--- frontend-js/src/lib/axios-instance.ts
+++ frontend-js/src/lib/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://ecommerce1-3fr4.vercel.app",
+    baseURL: "https://ecommerce1-wytm.onrender.com",
     withCredentials: true,  
 });
 
